7279805e3dfcbb7e8db0e6affa54aa67c28dc2b9
Adrianomssilva <adriano.mssilva1@gmail.com>
2023-08-14 20:45:05 -0300

    updte done

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -24,9 +24,23 @@ class CustomersController < ApplicationController
    
 
    def show
-   @customer = Customer.find(params[:id])
+      @customer = Customer.find(params[:id])
    end
 
+   def edit
+      @customer = Customer.find(params[:id])
+   end
+
+   def update
+      @customer = Customer.find(params[:id])
+
+      if @customer.update(customer_params)
+         redirect_to @customer
+      
+      else
+      render :edit
+      end
+   end
 
 
 
