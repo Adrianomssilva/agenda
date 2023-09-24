ba353967d123565d6079aa0f82ce7f2b74cd4e5a
Adrianomssilva <adriano.mssilva1@gmail.com>
2023-09-23 21:50:56 -0300

    params criados

--- teste.rb
+++ teste.rb
@@ -1,5 +1,5 @@
-class CustomerController << ApplicantionController
-
+class CustomersController < ApplicationController 
+  
   def index
     @customers = Customer.all
   end
@@ -9,11 +9,11 @@ class CustomerController << ApplicantionController
   end
 
   def create
-    @customer = Customer.new(customer_params)
-    if @customer.save
-      redirect_to customers_path    
-    else
-    render :new, status: :unprocessable_entity
+    @customer = Customer.new
+    if @customer.save(customer_params)
+      redirect_to customers_path
+    else 
+      render :new
     end
   end
 
@@ -21,33 +21,11 @@ class CustomerController << ApplicantionController
     @customer = Customer.find(params[:id])
   end
 
-  def edit
-    @customer = Customer.find(params[:id])
-  end
-
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to customer_path
-    else
-    render :edit
-    end
-  end
-
-  def destroy
-    @customer = Customer.find(params[:id])
-    @customer.destroy
-    redirect_to customers_path
-  end
 
   private
-  def customer_params
-    params.require(:customer).permit(:name, :cell, :e_mail, :birthday)
-  end
-end
 
-        <div class = "filter"> <%= form_with url: search_month_customers_path, method: :get do |form| %>
-        <%= form.label :month, "Buscar por:" %>
-        <%= form.select :month, (1..12) %>
-        <%= form.submit "Filtrar" %>
-        <% end %> </div> 
+  def customer_params
+    params.require(:customer).permit(:name, :email, :cell, :birthday)
+    
+
+end
