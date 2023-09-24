846bbc96f713e3d092f2cf50862b73222ef0b66d
Adrianomssilva <adriano.mssilva1@gmail.com>
2023-09-23 22:02:32 -0300

    crud pronto

--- teste.rb
+++ teste.rb
@@ -9,8 +9,8 @@ class CustomersController < ApplicationController
   end
 
   def create
-    @customer = Customer.new
-    if @customer.save(customer_params)
+    @customer = Customer.new(customer_params)
+    if @customer.save
       redirect_to customers_path
     else 
       render :new
@@ -21,11 +21,34 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to customers_path
+  end
+
 
   private
 
   def customer_params
     params.require(:customer).permit(:name, :email, :cell, :birthday)
-    
+  end
+
+  def fetch_customer
+    @customer = Customer.find(params[:id])
+  end
 
 end
