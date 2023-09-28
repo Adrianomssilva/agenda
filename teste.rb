288fcddefc79e7d2d4d773269c0be7ed18647444
devadrsilva <adriano.mssilva1@gmail.com>
2023-09-28 19:42:39 -0300

    teste

--- teste.rb
+++ teste.rb
@@ -1,51 +1,7 @@
 class CustomersController < ApplicationController 
   
-  before_action :fetch_customer, only: [:edit, :update, :show, :destroy]
-  def index
-    @customers = Customers.all
-  end
-
-  def new
-    @customer = Customer.new
-  end
-
-  def create
-    @customer = Customer.new(customer_params)
-    if @customer.save
-      redirect_to customers_path
-    else 
-      render :new
-    end
-  end
-
-  def show;  end  asdasda
-
-  def edit;  end
-
-  def update
-    @customer = Customer.find(params[:id])
-    if @customer.update(customer_params)
-      redirect_to customer_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @customer.destroy
-    redirect_to customers_path
-  end
-
-
-  private
-
-
-  def customer_params
-    params.require(:customer).permit(:name, :email, :cell, :birthday)
-  end
-
-  def fetch_customer
-    @customer = Customer.find(params[:id])
-  end
-
+def index
+  @customers = Customer.all
 end
+
+end!
