e30f6e1a2c9ec0dc9976776732289545a278c38b
devadrsilva <adriano.mssilva1@gmail.com>
2023-10-14 12:41:26 -0300

    new and create teste

--- teste.rb
+++ teste.rb
@@ -9,9 +9,11 @@ end
 teste teste teste teste teste tese teste
 
 def new
-  @customer = Customer.find(params[:id])
-  if @customer.new
-    redirect_to @customer_path
-  else 
-    render new 
+  @customer = Customer.new
+end
+
+def create 
+  @customer = Customer.new(customers_params)
+  redirect_to customer_path if customer.save
+  render :new
 end
